253c5cd451943eca443429bb5c6bac2e8280fdf8
monxchrome <inacheat@gmail.com>
2023-11-27 23:33:31 +0100

    fixed

--- my-app/App.js
+++ my-app/App.js
@@ -1,13 +1,21 @@
 import React, { useCallback, useState } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import AudioRecorder from './components/AudioRecorder/AudioRecorder';
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <AudioRecorder />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
